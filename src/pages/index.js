e6e60df772530e958b24de121ddbb2b5808021ca
Lope Ariyo <lopeariyo@gmail.com>
2020-04-06 21:06:38 +0100

    Created inital project set up

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,26 +1,6 @@
 import React from "react"
-import { Link } from "gatsby"
+import "../assets/scss/main.scss"
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
-
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    {/* <Helmet /> */}
-    {/* <Navbar /> */}
-    {/* <SideNavbar>*/}
-    {/* {children} */}
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-    {/* <Footer /> */}
-  </Layout>
-)
+const IndexPage = () => <h1>This is the start of my portfolio</h1>
 
 export default IndexPage
