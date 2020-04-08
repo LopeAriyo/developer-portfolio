ec61953147d346d2c37ce03f526d898594464f1d
Lope Ariyo <lopeariyo@gmail.com>
2020-04-08 08:23:15 +0100

    Styled side navbar

--- src/components/primary/SideNavbar.js
+++ src/components/primary/SideNavbar.js
@@ -1,6 +1,46 @@
 import React from "react"
 import { Link } from "gatsby"
 
-const sideNavbar = props => <nav className="side-navigation">Hello</nav>
+const sideNavbar = props => {
+  let navClassName = "side-navigation"
+
+  if (props.visible) {
+    navClassName = "side-navigation open"
+  }
+  return (
+    <nav className={navClassName}>
+      <div className="cross-icon">
+        <span className="cross-icon__line"></span>
+      </div>
+      <div className="logo">
+        <div className="logo-img">
+          <Link to="/#">
+            <img src="../../assets/images/gatsby-icon.png" />
+          </Link>
+        </div>
+      </div>
+      <div>
+        <ul className="side-nav-links">
+          <li className="side-nav-link">
+            <Link to="/#">Link 1 </Link>
+          </li>
+          <li className="side-nav-link">
+            <Link to="/#">Link 2 </Link>
+          </li>
+          <li className="side-nav-link">
+            <Link to="/#">Link 3 </Link>
+          </li>
+          <li className="side-nav-link">
+            <Link to="/#">Link 4 </Link>
+          </li>
+        </ul>
+      </div>
+      <div className="spacer"></div>
+      <div className="social-media">
+        This is where social media icons will go
+      </div>
+    </nav>
+  )
+}
 
 export default sideNavbar
