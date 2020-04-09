821da8ae17927582ef83ea0eff6073883f5d1fd4
Lope Ariyo <lopeariyo@gmail.com>
2020-04-09 20:15:40 +0100

    Change meta data title, using Gatsby.  Version 1.2 Complete.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,24 @@
 import React from "react"
+import { Helmet } from "react-helmet"
 import Layout from "../components/primary/Layout"
 
 const IndexPage = () => (
-  <Layout>
-    {/* <div id="wrapper">
+  <div className="application">
+    <Helmet>
+      <meta charSet="utf-8" />
+      <title>Lope Ariyo</title>
+      {/* <link rel="canonical" href="http://mysite.com/example" /> */}
+    </Helmet>
+
+    <Layout>
+      {/* <div id="wrapper">
       <Introduction />
       <Features />
       <Capabilities />
       <Contact />
     </div>*/}
-  </Layout>
+    </Layout>
+  </div>
 )
 
 export default IndexPage
