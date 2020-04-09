7f1ff7ba462a88ad687d2c613b6668a7f3a9e231
Lope Ariyo <lopeariyo@gmail.com>
2020-04-09 18:36:03 +0100

    Moved intro higher up for other views & changed site title. Version 1.0 Complete.

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,8 +18,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `lope-ariyo-developer`,
+        short_name: `la-dev`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
