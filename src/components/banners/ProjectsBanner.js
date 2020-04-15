e280cfb343e4a157dfceafbaf5b1f3f868c969d1
Lope Ariyo <lopeariyo@gmail.com>
2020-04-15 22:04:04 +0100

    Added blob to about banner

--- src/components/banners/ProjectsBanner.js
+++ src/components/banners/ProjectsBanner.js
@@ -4,8 +4,34 @@ const ProjectsBanner = props => (
   <div className="projects-banner">
     {/* <div id="small-squiggle"></div>
     <div id="small-squiggle"></div>
-    <div id="large-squiggle"></div>
-    <div id="blob"></div> */}
+    <div id="large-squiggle"></div> */}
+    <div id="wrapper">
+      <div className="blob">
+        <div className="ball"></div>
+        <div className="ball"></div>
+        <div className="ball"></div>
+      </div>
+
+      <svg>
+        <defs>
+          <filter id="filter">
+            <feGaussianBlur
+              in="SourceGraphic"
+              stdDeviation="18"
+              result="blur"
+            />
+            <feColorMatrix
+              in="blur"
+              mode="matrix"
+              values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 28 -10"
+              result="filter"
+            />
+            <feComposite in="SourceGraphic" in2="filter" operator="atop" />
+          </filter>
+        </defs>
+      </svg>
+    </div>
+
     <div className="section">
       <p className="title">Projects</p>
       <p className="info">
