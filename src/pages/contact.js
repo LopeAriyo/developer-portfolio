ff337b0089dd7036c74f8c0c532ee3d36b59c41a
Lope Ariyo <lopeariyo@gmail.com>
2020-06-16 16:24:42 +0100

    removed extra space in message

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -12,43 +12,40 @@ const NotFoundPage = () => {
           <p>Need to get in touch with me? I&#39;m all ears.</p>
           <br />
           <br />
-          <article className="contact-form">
-            <h4>Want to send a message</h4>
-            <form
-              name="Contact Form"
-              method="POST"
-              netlify-honeypot="bot-field"
-              data-netlify="true"
-            >
-              <input type="hidden" name="form-name" value="Contact Form" />
-              <div className="form-group">
-                <input
-                  className="form-control"
-                  type="text"
-                  name="name"
-                  placeholder="Name"
-                />
-              </div>
-              <div>
-                <input
-                  className="form-control"
-                  type="email"
-                  name="email"
-                  placeholder="Email"
-                />
-              </div>
-              <div>
-                <textarea
-                  className="form-control"
-                  name="message "
-                  placeholder="Message"
-                  rows="8"
-                ></textarea>
-              </div>
-              <input type="hidden" name="bot-field" />
-              <button type="submit">Email Me</button>
-            </form>
-          </article>
+          <h4>Want to send a message</h4>
+          <form
+            className="form-group"
+            name="Contact Form"
+            method="POST"
+            data-netlify="true"
+          >
+            <input type="hidden" name="form-name" value="Contact Form" />
+            <div>
+              <input
+                className="form-control"
+                type="text"
+                name="name"
+                placeholder="Name"
+              />
+            </div>
+            <div>
+              <input
+                className="form-control"
+                type="email"
+                name="email"
+                placeholder="Email"
+              />
+            </div>
+            <div>
+              <textarea
+                className="form-control"
+                name="message"
+                placeholder="Message"
+                rows="8"
+              ></textarea>
+            </div>
+            <button type="submit">Email Me</button>
+          </form>
           <h4>Want to meet instead?</h4>
           <Link to="/">
             <button>Meet Me</button>
