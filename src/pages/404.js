f0d0e6019c67cf717e46e26be47e3fce3c7fcc1a
Lope Ariyo <lopeariyo@gmail.com>
2020-06-16 11:31:41 +0100

    created error page with styling

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,27 @@
 import React from "react"
+import Layout from "../components/primary/Layout"
+import { Link } from "gatsby"
+import Blob from "../components/secondary/Blob"
 
-const NotFoundPage = () => (
-  <div>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </div>
-)
+const NotFoundPage = () => {
+  return (
+    <Layout>
+      <main className="error-page">
+        <Blob />
+        <div className="error-container">
+          <h1>Ooops...</h1>
+          <h4> 404 - Page not found! </h4>
+          <p>
+            Sorry but the page you are looking for doesn&#39;t exist, may have
+            been removed, name changed or is temporarily unavailable.
+          </p>
+          <Link to="/">
+            <button>Back Home</button>
+          </Link>
+        </div>
+      </main>
+    </Layout>
+  )
+}
 
 export default NotFoundPage
